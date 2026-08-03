5974cb704ac2f26101fe9fc0bbe6a51ff0676603
unknown <deripernandi99@gmail.comgit config --globam user.name Deri>
2026-08-03 16:33:15 +0700

    app.py

--- app.py
+++ app.py
@@ -444,7 +444,7 @@ def get_tiktok_auth_url():
     params = {
         "client_key": TIKTOK_CLIENT_KEY,
         "response_type": "code",
-        "scope": "user.info.basic,video.upload",
+        "scope": "user.info.basic,video.upload,video.publish",
         "redirect_uri": redirect_uri,
         "state": state
     }
