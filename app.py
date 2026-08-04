da1f5f6daafcba5cf9e1b0da6cf9dedd54b0a3ef
unknown <deripernandi99@gmail.comgit config --globam user.name Deri>
2026-08-04 16:21:26 +0700

    app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ def tos_page(): return render_template('tos.html')
 @app.route('/privacy')
 def privacy_page(): return render_template('privacy.html')
 
-@app.route('/data_deletion_page')
+@app.route('/data-deletion')
 def data_deletion_page(): 
     """Route baru untuk halaman panduan penghapusan data (Wajib untuk Meta App Review)."""
     return render_template('data_deletion_page.html')
