4c9372e16628ef65878d0fd4357213d66060c9fb
unknown <deripernandi99@gmail.comgit config --globam user.name Deri>
2026-08-08 10:26:18 +0700

    Memecah kode app.py jadi rapi

--- app.py
+++ app.py
@@ -38,36 +38,7 @@ except ImportError:
 app = Flask(__name__)
 CORS(app) 
 
-GOOGLE_SHEET_ID = "1P0zTEwtMmWfxhHAY6-QbQd5to6Id1rzazgel-PiSJwI" 
-SERVICE_ACCOUNT_FILE = "service_account.json" 
-
-user_2fa_store = {}
-
-# KONFIGURASI TIKTOK DEVELOPER
-TIKTOK_CLIENT_KEY = os.environ.get("TIKTOK_CLIENT_KEY")
-TIKTOK_CLIENT_SECRET = os.environ.get("TIKTOK_CLIENT_SECRET")
-
-# KONFIGURASI META (FACEBOOK & INSTAGRAM)
-META_CLIENT_ID = os.environ.get("META_CLIENT_ID")
-META_CLIENT_SECRET = os.environ.get("META_CLIENT_SECRET")
-META_WEBHOOK_VERIFY_TOKEN = os.environ.get("META_WEBHOOK_VERIFY_TOKEN", "trendora_meta_secret_123")
-
-# KONFIGURASI LINKEDIN
-LINKEDIN_CLIENT_ID = os.environ.get("LINKEDIN_CLIENT_ID")
-LINKEDIN_CLIENT_SECRET = os.environ.get("LINKEDIN_CLIENT_SECRET")
-
-# KONFIGURASI YOUTUBE / GOOGLE
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
-
-# KONFIGURASI THREADS
-THREADS_CLIENT_ID = os.environ.get("THREADS_CLIENT_ID")
-THREADS_CLIENT_SECRET = os.environ.get("THREADS_CLIENT_SECRET")
-
-# KONFIGURASI TWITTER
-TWITTER_CLIENT_ID = os.environ.get("TWITTER_CLIENT_ID")
-TWITTER_CLIENT_SECRET = os.environ.get("TWITTER_CLIENT_SECRET")
-
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 def get_gsheet():
     """Koneksi ke Google Sheets (Sheet Utama)."""
@@ -269,10 +240,7 @@ def db_get_twitter_token_by_api_key(api_key):
         except Exception as e:
             print(f"GSheet Get Twitter Token Error: {e}")
     return None
-
-
-SMTP_EMAIL = os.environ.get("SMTP_EMAIL", "trendoraautomation@gmail.com") 
-SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD", "") 
+ 
 
 def send_email_qr(recipient_email, qr_url, secret):
     if not SMTP_PASSWORD: return False
@@ -324,12 +292,6 @@ def verify_totp(secret, token):
         if get_totp_token(secret, curr_interval + delta) == str(token).strip():
             return True
     return False
-
-# ==========================================
-# KONFIGURASI MIDTRANS (PRODUCTION)
-# ==========================================
-MIDTRANS_API_URL = "https://app.midtrans.com/snap/v1/transactions"
-MIDTRANS_SERVER_KEY = os.environ.get("MIDTRANS_SERVER_KEY", "Mid-server-zF-SefFUBo7r1t-qcRPzdBEr_DUMMY")
 
 
 @app.route('/')
---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+#konfigurasi gsheet
+GOOGLE_SHEET_ID = "1P0zTEwtMmWfxhHAY6-QbQd5to6Id1rzazgel-PiSJwI" 
+SERVICE_ACCOUNT_FILE = "service_account.json"
+
+# KONFIGURASI TIKTOK DEVELOPER
+TIKTOK_CLIENT_KEY = os.environ.get("TIKTOK_CLIENT_KEY")
+TIKTOK_CLIENT_SECRET = os.environ.get("TIKTOK_CLIENT_SECRET")
+
+# KONFIGURASI META (FACEBOOK & INSTAGRAM)
+META_CLIENT_ID = os.environ.get("META_CLIENT_ID")
+META_CLIENT_SECRET = os.environ.get("META_CLIENT_SECRET")
+META_WEBHOOK_VERIFY_TOKEN = os.environ.get("META_WEBHOOK_VERIFY_TOKEN", "trendora_meta_secret_123")
+
+# KONFIGURASI LINKEDIN
+LINKEDIN_CLIENT_ID = os.environ.get("LINKEDIN_CLIENT_ID")
+LINKEDIN_CLIENT_SECRET = os.environ.get("LINKEDIN_CLIENT_SECRET")
+
+# KONFIGURASI YOUTUBE / GOOGLE
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
+
+# KONFIGURASI THREADS
+THREADS_CLIENT_ID = os.environ.get("THREADS_CLIENT_ID")
+THREADS_CLIENT_SECRET = os.environ.get("THREADS_CLIENT_SECRET")
+
+# KONFIGURASI TWITTER
+TWITTER_CLIENT_ID = os.environ.get("TWITTER_CLIENT_ID")
+TWITTER_CLIENT_SECRET = os.environ.get("TWITTER_CLIENT_SECRET")
+
+# ==========================================
+# KONFIGURASI MIDTRANS (PRODUCTION)
+# ==========================================
+MIDTRANS_API_URL = "https://app.midtrans.com/snap/v1/transactions"
+MIDTRANS_SERVER_KEY = os.environ.get("MIDTRANS_SERVER_KEY", "Mid-server-zF-SefFUBo7r1t-qcRPzdBEr_DUMMY")
+
+SMTP_EMAIL = os.environ.get("SMTP_EMAIL", "trendoraautomation@gmail.com") 
+SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD", "")
+
+#GEMINI API KEY
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+
+user_2fa_store = {}
