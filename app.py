deb7798a989c09707805364482df496d4993f7fe
unknown <deripernandi99@gmail.comgit config --globam user.name Deri>
2026-08-11 20:31:13 +0700

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-"""
-===============================================================================
-app.py - Main Application Entrypoint
-===============================================================================
-Entrypoint utama Flask Trendora Automation.
-Semua routing dialihkan ke folder /routes menggunakan Blueprint.
-"""
-
 from flask import Flask
 from flask_cors import CORS
 from routes import register_routes
@@ -13,7 +5,6 @@ from routes import register_routes
 app = Flask(__name__)
 CORS(app)
 
-# Registrasi seluruh Blueprint dari package routes
 register_routes(app)
 
 if __name__ == '__main__':
